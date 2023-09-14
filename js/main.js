08fe3fe6e64e71cd3d7bf5d260801a9d48077fac
crocodileDaniil <karasev_dan_an@mail.ru>
2023-09-14 23:36:53 +0300

    lesson1-end

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".bike__slider").slick({
+    //   fade: true,
+    arrows: false,
+    dots: true,
+    fade:true,
+    autoplay:true,
+    autoplaySpeed:2000
+  });
+});
